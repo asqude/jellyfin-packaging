63aba0b079ad50d04ad4178d411c282f28db0493
asqude <65425758+asqude@users.noreply.github.com>
2025-12-15 03:17:02 +0100

    add ability to build package from branch

--- checkout.py
+++ checkout.py
@@ -40,21 +40,40 @@ for submodule in this_repo.submodules:
     submodules[submodule.name] = submodule.module()
 
 # Validate that the provided tag is valid; if not, fall back to "master"
+is_tag = True
 if target_release != "master":
     if (
         target_release not in submodules["jellyfin-server"].tags
         or target_release not in submodules["jellyfin-web"].tags
     ):
-        print(
-            f"WARNING: Provided tag {target_release} is not a valid tag for both jellyfin-server and jellyfin-web; using master instead"
-        )
-        target_release = "master"
+        # Check if it is a branch instead
+        is_branch = True
+        for module in ["jellyfin-server", "jellyfin-web"]:
+            found = False
+            for ref in submodules[module].remotes.origin.refs:
+                if ref.remote_head == target_release:
+                    found = True
+                    break
+            if not found:
+                is_branch = False
+                break
+        
+        if is_branch:
+            is_tag = False
+            print(f"Found branch {target_release} in submodules")
+        else:
+            print(
+                f"WARNING: Provided release {target_release} is not a valid tag or branch for both jellyfin-server and jellyfin-web; using master instead"
+            )
+            target_release = "master"
 
 for submodule in submodules.keys():
     if target_release == "master" or submodule == 'jellyfin-server-windows':
         target_head = "origin/master"
-    else:
+    elif is_tag:
         target_head = f"refs/tags/{target_release}"
+    else:
+        target_head = f"origin/{target_release}"
     # Checkout the given head and reset the working tree
     submodules[submodule].head.reference = target_head
     submodules[submodule].head.reset(index=True, working_tree=True)
